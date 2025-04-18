98c003e50c716b46eea0c6ef5f4b239ba6493e6b
Jean LE CHAUDELEC <132900223+Jeanlc-citations@users.noreply.github.com>
2025-04-18 19:32:52 +0200

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 function Footer() {
-    return <footer style={{ textAlign: "center", padding: "10px", fontSize: "14px", color: "#555" }}>© 2025 - Histoire de France</footer>
-  }
-  export default Footer;
+  return (
+    <footer style={{ textAlign: "center", padding: "10px", fontSize: "14px", color: "#555" }}>
+      Créé par Jean Le Chaudelec | Cours de Programmation Web d'Antoine Libert
+    </footer>
+  );
+}
+  
+export default Footer;
   
